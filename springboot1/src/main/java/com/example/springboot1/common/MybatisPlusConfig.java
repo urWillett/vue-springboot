c033e5260bd0afb4e9187115a36e6fff054b9fbc
urWillett <2422751954@qq.com>
2021-11-11 17:36:15 +0800

    MybatisPlusConfig修改

--- springboot1/src/main/java/com/example/springboot1/common/MybatisPlusConfig.java
+++ springboot1/src/main/java/com/example/springboot1/common/MybatisPlusConfig.java
@@ -1,17 +1,24 @@
 package com.example.springboot1.common;
 //mybatis-plus 分页插件
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@MapperScan("com.baomidou.cloud.service.*.mapper*")
 public class MybatisPlusConfig {
     /**
-     * 分页插件
+     * 分页插件  mybatis-plus  官网引入
      */
 
     @Bean
-    public paginationInterceptor paginationInterceptor() {return new PaginationInnerInterceptor
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
     }
 }
